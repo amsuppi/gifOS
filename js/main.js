4399e11dcc14bc0dd1d8b696390f0b79a88c0084
Mari <maribelsuppi97@gmail.com>
2020-03-13 09:18:56 -0300

    js modificaciones

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const common = 'styles/styles.css'
+const black = 'styles/stylesDark.css'
+
 function dropFunction(){
     var content = document.getElementById("drop1");
 
@@ -8,9 +11,13 @@ function dropFunction(){
     } 
  }
 
+function cambiarEstilos(id){
+    if (id == "opcionRosa"){
+        document.getElementById("archivoCss").href = common;
+    } else{
+        document.getElementById("archivoCss").href = black;
+    }
 
-function cambiarEstilos(archivo){
-    document.getElementById("archivoCss").href = archivo;
 }
 
 
