0172f9bd39a866bea36bc9d15d015318ce5477e8
Maribel <maribelsuppi97@gmail.com>
2020-03-23 00:40:09 -0300

    Ventana emergente gris
    Grabar video, modificacion de api, S

--- js/api.js
+++ js/api.js
@@ -2,23 +2,24 @@ let apiKey = "cq2dHiXUIQsux4tIWZYG7Mj0p0HHzicx"
 var miUrl = fetch("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=cq2dHiXUIQsux4tIWZYG7Mj0p0HHzicx");
 
 //------------------------------------
-
+ var boton =  document.getElementById("search");
  
 
 miUrl .then(function(data) { 
+    for (let i = 0; i < data.length; i++) {
+        createImageBitmap(data[i].images.original.url);
+        
+    }
     console.log(data); 
     
 });
 
+boton.addEventListener("click", function(){
+    var miInfo = document.getElementById("n2").value;
 
+    console.log 
 
-//------------- BUSCAR ----------------
-
-function llamar(){
-    var info = document.getElementById("n2").value;
-
-    console.log(info);
-}
+});
 
 
 function getSearchResults(search) {
--- js/video.js
+++ js/video.js
@@ -1,23 +1,52 @@
-function getStreamAndRecord(){
-
+//Activar la camara de la computadora
+function activarCamara() {
     navigator.mediaDevices.getUserMedia({
-        audio:false,
+        audio: false,
         video: {
             height: {
-                max:400
+                max: 400
             }
         }
-    }).then(function(stream){
+    }).then(function (stream) {
         var video = document.querySelector('video');
         video.srcObject = stream;
-        
-        video.onloadeddata = function(e){
+        video.onloadeddata = function (e) {
             video.play();
         }
-
-    })/*.catch(function (err) {
-
-        console.log(err.name + ":" + err.message);
-        
-    })*/
+    })
 }
+
+
+
+// Libreria RTC para grabar video
+    function grabar() {
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        }).then(async function(stream) {
+            recorder = RecordRTC(stream, {
+                type: 'gif',
+                frameRate: 1,
+                quality: 10,
+                width: 360,
+                hidden: 240,
+                
+                onGifRecordingStarted: function() {
+                 console.log('started')
+               },
+              }); 
+            recorder.startRecording();
+        
+            const sleep = m => new Promise(r => setTimeout(r, m));
+            await sleep(3000);
+        
+            recorder.stopRecording(function() {
+                let blob = recorder.getBlob();
+                invokeSaveAsDialog(blob);
+            });
+        });
+    }
+
+    
+
+ 
