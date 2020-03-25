e9903bd6120af07e74c4d09509fc073e317e8a6e
Maribel <maribelsuppi97@gmail.com>
2020-03-24 21:31:42 -0300

    gif de sugerencias

--- js/api.js
+++ js/api.js
@@ -1,14 +1,31 @@
-var miUrl = "//api.giphy.com/v1/gifs/trending?api_key=NXLRwlrJclD68iUt0t49LYzzurK0KJxq&limit=25&rating=G"
+var trending = "https://api.giphy.com/v1/gifs/trending?api_key=NXLRwlrJclD68iUt0t49LYzzurK0KJxq&limit=25&rating=G"
+var sugerencias = "https://api.giphy.com/v1/gifs/translate?api_key=NXLRwlrJclD68iUt0t49LYzzurK0KJxq&s="
 let apiKey = "NXLRwlrJclD68iUt0t49LYzzurK0KJxq"
 //------------------------------------
+    fetch(trending)
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            console.log(json);
 
-fetch (miUrl)
-.then(response => {
-    return response.json();
-}).then(json => {
-    console.log(json);
-});
+            for (let i = 0; i < json.data.length; i++) {
+                console.log(json.data[i].images.original.url);
 
+               document.getElementById("contenedo-trending").src = json.data[0].images.original.url;
+                document.getElementById("contenedo-trending2").src = json.data[1].images.original.url;
+                document.getElementById("contenedo-trending3").src = json.data[2].images.original.url;
+                document.getElementById("contenedo-trending4").src = json.data[3].images.original.url;
+
+            }
+
+        })
+        .catch(error => {
+            return error;
+        });
+
+
+        
 
 //search
 /*var boton =  document.getElementById("search");
